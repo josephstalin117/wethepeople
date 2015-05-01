49932a120c93d409e02db9549f533e2321a02049
josephstalin <lyz2356002@gmail.com>
2015-05-01 19:07:52 +0800

    修改了数据库结构

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -5,12 +5,12 @@ use yii\db\Migration;
 
 class m130524_201442_init extends Migration
 {
-    public function up()
+    /*public function up()
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=MyISAM';
         }
 
         $this->createTable('{{%user}}', [
@@ -30,5 +30,96 @@ class m130524_201442_init extends Migration
     public function down()
     {
         $this->dropTable('{{%user}}');
+    }*/
+
+
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=MyISAM';
+        }
+
+        //user
+        $this->createTable('{{%user}}', [
+            'id' => Schema::TYPE_PK,
+            'username' => Schema::TYPE_STRING . ' NOT NULL',
+            'auth_key' => Schema::TYPE_STRING . '(32) NOT NULL',
+            'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
+            'password_reset_token' => Schema::TYPE_STRING,
+            'email' => Schema::TYPE_STRING . ' NOT NULL',
+
+            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
+            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ], $tableOptions);
+
+        //stu_info
+        $this->createTable('{{%stu_info}}', [
+            'id' => Schema::TYPE_PK,
+            'user_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'real_name' => Schema::TYPE_STRING . ' NOT NULL',
+        ], $tableOptions);
+
+        //tea_info
+        $this->createTable('{{%tea_info}}', [
+            'id' => Schema::TYPE_PK,
+            'user_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'real_name' => Schema::TYPE_STRING . ' NOT NULL',
+        ], $tableOptions);
+
+        //suggestion
+        $this->createTable('{{%suggestion}}', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'content' => Schema::TYPE_TEXT . ' NOT NULL',
+            'submitter' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 0',
+
+            'up' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'down' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
+            'part' => Schema::TYPE_TEXT,
+
+            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ], $tableOptions);
+
+        $this->createTable('{{%sugges_detail}}', [
+            'id' => Schema::TYPE_PK,
+            'sugg_id' => Schema::TYPE_INTEGER,
+            'part_id' => Schema::TYPE_INTEGER,
+            'attitude' => Schema::TYPE_INTEGER,
+        ], $tableOptions);
+
+        $this->createTable('{{%comment}}', [
+            'id' => Schema::TYPE_PK,
+            'sugg_id' => Schema::TYPE_INTEGER,
+            'part_id' => Schema::TYPE_INTEGER,
+            'content' => Schema::TYPE_TEXT,
+            'time' => Schema::TYPE_INTEGER,
+        ], $tableOptions);
+
+        $this->createTable('{{%message}}',[
+            'id'=>Schema::TYPE_PK,
+            'send_id'=>Schema::TYPE_INTEGER,
+            'recive_id'=>Schema::TYPE_INTEGER,
+            'un_read'=>Schema::TYPE_INTEGER,
+            'content'=>Schema::TYPE_TEXT,
+            'time'=>Schema::TYPE_INTEGER,
+        ],$tableOptions);
+
     }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+        $this->dropTable('{{%stu_info}}');
+        $this->dropTable('{{%tea_info}}');
+        $this->dropTable('{{%suggestion}}');
+        $this->dropTable('{{%suggestion}}');
+        $this->dropTable('{{%suggestion}}');
+    }
+
 }
