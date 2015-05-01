4ae64942d63e5162bbc3313d337c206e39ab5255
josephstalin <lyz2356002@gmail.com>
2015-05-02 02:08:02 +0800

    add the controller of suggestion

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -4,7 +4,7 @@ namespace backend\controllers;
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
-use common\models\LoginForm;
+use app\models\LoginForm;
 use yii\filters\VerbFilter;
 
 /**
@@ -66,7 +66,7 @@ class SiteController extends Controller
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
+            return $this->goHome();
         } else {
             return $this->render('login', [
                 'model' => $model,
