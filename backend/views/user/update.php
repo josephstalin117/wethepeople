c778bce6b6fb475e382835c8c8d3b41630973551
josephstalin <lyz2356002@gmail.com>
2015-05-24 02:39:39 +0800

    修改了语言汉化

--- backend/views/user/update.php
+++ backend/views/user/update.php
@@ -6,11 +6,11 @@ use yii\helpers\Html;
 /* @var $model common\models\User */
 
 $this->title = Yii::t('app', '修改 {modelClass}: ', [
-    'modelClass' => 'User',
+    'modelClass' => '用户',
 ]) . ' ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Users'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', '用户'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+$this->params['breadcrumbs'][] = Yii::t('app', '更新');
 ?>
 <div class="user-update">
 
